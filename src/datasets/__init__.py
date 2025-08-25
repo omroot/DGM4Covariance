9a8bf679ed763fe51cbc13a638816ff9b8ecc945
Oualid Missaoui <oualid.missaoui@gmail.com>
2025-08-25 18:06:52 -0400

    update

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,4 +1,3 @@
-from . block_matrix import BlockMatrixGenerator
 from . mean_covariance_generator import build_block_matrix
 from . mean_covariance_generator import build_true_matrix
 from . mean_covariance_generator import simulate_from_covariance_mean
